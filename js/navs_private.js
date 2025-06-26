6128ce38bfedc59d2324f70d59247bb56db2bf5d
Melanie San Roman <srmel123@gmail.com>
2025-06-25 22:52:41 -0300

    Se creo el archivo js encargado de la barra de navegacion privada de la pagina

---
+++ js/navs_private.js
@@ -0,0 +1,15 @@
+document.getElementById("link_index").addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+
+document.getElementById("link_profile").addEventListener("click", () => {
+  window.location.href = "profile.html";
+});
+
+document.getElementById("link_reservations").addEventListener("click", () => {
+  window.location.href = "reservations.html";
+});
+
+document.getElementById("link_search").addEventListener("click", () => {
+  window.location.href = "search.html";
+});
