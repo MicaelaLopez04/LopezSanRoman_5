c83519ae9691f104ce46cab5a1229fd93746b03b
Melanie San Roman <srmel123@gmail.com>
2025-06-25 22:53:48 -0300

    Se creo el archivo js encargado de la barra de navegacion publica de la pagina

---
+++ js/navs_public.js
@@ -0,0 +1,19 @@
+document.getElementById("btn_index").addEventListener("click", () => {
+    window.location.href = "index.html"; // Cambiá por tu ruta real
+});
+
+document.getElementById("btn_questions").addEventListener("click", () => {
+    window.location.href = "preguntas.html"; // Cambiá por la página de preguntas frecuentes
+});
+
+document.getElementById("btn_search").addEventListener("click", () => {
+    window.location.href = "search.html"; // Página de búsqueda
+});
+
+document.getElementById("btn_info").addEventListener("click", () => {
+    window.location.href = "info.html"; // Página de información
+});
+
+document.getElementById("btn_miperfil").addEventListener("click", () => {
+    window.location.href = "profile.html"; // Tu perfil de usuario
+});
